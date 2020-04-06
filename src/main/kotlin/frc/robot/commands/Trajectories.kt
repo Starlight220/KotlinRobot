821ffe5d6c57f8b5f7399828cb40a7bd2d18f28c
Starlight220 <yotamshlomi@gmail.com>
2020-04-06 23:32:00 +0300

    add pathfinding

--- src/main/kotlin/frc/robot/commands/Trajectories.kt
+++ src/main/kotlin/frc/robot/commands/Trajectories.kt
@@ -1,24 +1,55 @@
 package frc.robot.commands
 
+import edu.wpi.first.wpilibj.Filesystem
+import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.trajectory.Trajectory
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
 import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil
 import java.io.IOException
-import java.nio.file.Path
 import edu.wpi.first.wpilibj.DriverStation as DS
 
 
-object Trajectories{
-    private val map : Map<String, String> = mapOf(
-            Pair("reverseShoot","reverseShoot.json"),
-            Pair("forwardShoot","forwardShoot.json")
+object Trajectories {
+    private val map: Map<String, TrajectorySource> = mapOf(
+            Pair("reverseShoot", JsonTrajectory("reverse")),
+            Pair("forwardShoot", JsonTrajectory("fwd")),
+            Pair("null", NullTrajectory)
     )
 
-    operator fun get(name : String) : Trajectory{
-        return try {
-            TrajectoryUtil.fromPathweaverJson(Path.of(map[name]))
-        }catch (e : IOException){
-            DS.reportError("trajectory $name generation failed : ${e.message}", false)
-            Trajectory(listOf())
+    operator fun get(name: String): Trajectory {
+        val src = (map[name]) ?: NullTrajectory
+        return src.getTrajectory()
+    }
+
+    interface TrajectorySource {
+        fun getTrajectory(): Trajectory
+    }
+
+    object NullTrajectory : Trajectory(listOf()), TrajectorySource{
+        override fun getTrajectory(): Trajectory {
+            return this
         }
+    }
+
+    class JsonTrajectory(private val name: String) : TrajectorySource {
+        override fun getTrajectory(): Trajectory {
+            return try {
+                val path = Filesystem.getDeployDirectory().toPath().resolve("output/$name.wpilib.json")
+                TrajectoryUtil.fromPathweaverJson(path)
+            } catch (e: IOException) {
+                DS.reportError("trajectory $name generation failed : ${e.message}", false)
+                return Trajectory(listOf())
+            }
+        }
+    }
+    class WpiTrajectory(private val states : List<Pose2d>) : TrajectorySource{
+        companion object {
+            private val config = TrajectoryConfig(10.0, 10.3)
+        }
+        override fun getTrajectory(): Trajectory {
+            return TrajectoryGenerator.generateTrajectory(states, config)
+        }
+
     }
 }
