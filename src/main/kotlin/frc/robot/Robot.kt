ff8180cfb38762fad1e8585405192baf06f07a2f
Starlight220 <yotamshlomi@gmail.com>
2020-05-10 23:33:47 +0300

    init trajectory work

--- src/main/kotlin/frc/robot/Robot.kt
+++ src/main/kotlin/frc/robot/Robot.kt
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 import edu.wpi.first.wpilibj2.command.RunCommand
 import frc.robot.commands.auto.goBackAndShoot
+import frc.robot.commands.auto.initTrajectories
 import frc.robot.commands.intake.IntakeDrive
 import frc.robot.commands.transport.TransportDrive
 import frc.robot.subsystems.*
@@ -43,6 +44,8 @@ object Robot : TimedRobot() {
         )
         Limelight
         subsystems.forEach { it() }
+
+        initTrajectories()
 //        autoCommand = initAutoCommand()
     }
 
--- src/main/kotlin/frc/robot/commands/auto/CommandGenerator.kt
+++ src/main/kotlin/frc/robot/commands/auto/CommandGenerator.kt
@@ -11,7 +11,6 @@ import lib.trajectory.TrajectorySource.Trajectories
 fun getShootProcess(shooter: () -> Boolean): Command =
         KeepVelocity(shootVelocity).alongWith(KeepAngle({ Limelight.tx }), IntoShooter(shooter))
 
-
 fun goBackAndShoot() : Command {
     val backUp = Drivetrain.getRamsete(Trajectories["goBackAndShoot"])
     val shootProcess = getShootProcess {
@@ -19,6 +18,6 @@ fun goBackAndShoot() : Command {
     }
     val collectBalls = Drivetrain.getRamsete(Trajectories["collectTrenchBalls"])
 
-    return backUp andThen shootProcess andThen collectBalls
+    return backUp.andThen(shootProcess, collectBalls)
 }
 
